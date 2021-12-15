741c3c52eab15c720253cfa3cdd77b4c5919f340
MaciejSzelag <szelag.maciej@gmail.com>
2021-12-15 23:23:53 +0000

    blog scss

--- blog.php
+++ blog.php
@@ -4,57 +4,67 @@
         <div class="blog-header">
             <div class="header-wrap">
                 <div class="page-title">
-                    <h1>Blog</h1>
+                    <h1>What is what?</h1>
                 </div>
             </div>
         </div>
         <main class="main-blog">
             <div class="main-wrap">
-                <div class="blog-box">
-                    <div class="box-img">
-                        <div class="img-wrap">
-                            <img src="" alt="">
+                <a href="">
+                    <div class="blog-box">
+                        <div class="box-img">
+                            <div class="img-wrap">
+                                <img src="images/laptop-634x640.png" alt="">
+                            </div>
                         </div>
-                    </div>
-                    <div class="box-content">
-                        <h1>Title</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p>
-                        <span>Date 12/12/2021</span>
-                    </div>
+                        <div class="box-content">
+                            <h1>Title</h1>
+                            <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p> -->
 
-
-
-                </div>
-                <div class="blog-box">
-                    <div class="box-img">
-                        <div class="img-wrap">
-                            <img src="" alt="">
+                           <p> <?php echo substr("Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?", 0,150);?>...</p>
+                            <span>Published: 12/12/2021</span>
                         </div>
+
+
+
                     </div>
-                    <div class="box-content">
-                        <h1>Title</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p>
-                        <span>Date 12/12/2021</span>
-                    </div>
+                </a>
 
-
-
-                </div>
-                <div class="blog-box">
-                    <div class="box-img">
-                        <div class="img-wrap">
-                            <img src="" alt="">
+                <a href="">
+                    <div class="blog-box">
+                        <div class="box-img">
+                            <div class="img-wrap">
+                                <img src="images/laptop-startup_640-640.png" alt="">
+                            </div>
                         </div>
+                        <div class="box-content">
+                            <h1>Title</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p>
+                            <span>Published: 12/12/2021</span>
+                        </div>
+
+
+
                     </div>
-                    <div class="box-content">
-                        <h1>Title</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p>
-                        <span>Date 12/12/2021</span>
+                </a>
+
+                <a href="">
+                    <div class="blog-box">
+                        <div class="box-img">
+                            <div class="img-wrap">
+                                <img src="images/portfolio_640.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="box-content">
+                            <h1>Title</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquid praesentium eaque laudantium saepe quo nulla, odio dolores tenetur doloremque in hic nostrum et porro veniam illo vitae. Nesciunt, hic?</p>
+                            <span>Published: 12/12/2021</span>
+                        </div>
+
+
+
                     </div>
-
-
-
-                </div>
+                </a>
 
 
 
